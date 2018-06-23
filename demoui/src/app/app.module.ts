dca66c76fb1d8ad9f4f191863ac7fa3050bb1927
gokul <gokulnathvadivel95@gmail.com>
2018-06-23 19:42:09 +0530

    Added the new component called server.

--- demoui/src/app/app.module.ts
+++ demoui/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { serverComponent } from './server/server.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    serverComponent
   ],
   imports: [
     BrowserModule,
